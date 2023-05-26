3162c5ee2f423c33cf20bfb07a2acd0667904c76
offirsarid <offirsarid@mail.tau.ac.il>
2023-05-26 21:59:03 +0300

    Create sample dir after flattening using data flattening function

--- data/data_flattening.py
+++ data/data_flattening.py
@@ -5,10 +5,10 @@ import shutil
 import openpyxl
 
 
-audio_source_dir = "path/voxceleb2_audio"
-video_source_dir = "path/voxceleb2_video"
-destination_dir = "path/voxceleb2"
-labels_file = "path/labels.xlsx"
+audio_source_dir = "audio_dir_sample_17_before_flattening"
+video_source_dir = "video_dir_sample_17_before_flattening"
+destination_dir = "sample_17_after_flattening"
+labels_file = "labels.xlsx"
 
 
 wb = openpyxl.Workbook()
@@ -22,7 +22,7 @@ def data_flattening(source_dir, destination_dir, file_extension, type, write_lab
     """ This function iterate source dir of voxceleb2 dataset
     and create samples directory with audio and video subdirectories.
     the function create xl file saving the sample label - speaker id."""
-    id_directories = glob.glob(path.join(source_dir, 'id*'))
+    id_directories = glob.glob(path.join(source_dir, "id*"))
     sample_num = 0
     row = 2
     # Iterate over id directories of audio/video data
@@ -35,7 +35,8 @@ def data_flattening(source_dir, destination_dir, file_extension, type, write_lab
         for id_sample in id_samples:
             # Creating the sample directories
             destination_sample_path = path.join(destination_dir, f'sample_{sample_num}')
-            makedirs(path.join(destination_sample_path, f'{type}'), exist_ok=True)
+            destination_sample_path = path.join(destination_sample_path, f'{type}')
+            makedirs(destination_sample_path, exist_ok=True)
             # Copy samples file from source
             shutil.copy(id_sample, path.join(destination_sample_path, f'sample_{sample_num}{file_extension}'))
             # Writing label for corresponding sample into xl file
@@ -45,9 +46,12 @@ def data_flattening(source_dir, destination_dir, file_extension, type, write_lab
                 row += 1
             sample_num += 1
 
+    if write_labels:
+        wb.save(labels_file)
 
-data_flattening(audio_source_dir, destination_dir, '.m4a', 'audio', True)
-data_flattening(video_source_dir, destination_dir, '.mp4', 'video', False)
+if __name__ == "__main__":
+    data_flattening(audio_source_dir, destination_dir, '.m4a', 'audio', True)
+    data_flattening(video_source_dir, destination_dir, '.mp4', 'video', False)
 
 
 
