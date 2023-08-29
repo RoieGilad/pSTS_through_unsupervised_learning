6ecd5e18b0d732b92b2e27f85d04b1714b11241c
RoieGilad <79976783+RoieGilad@users.noreply.github.com>
2023-08-29 08:50:18 +0300

    fixed slow neptune push

--- evaluate_script.py
+++ evaluate_script.py
@@ -86,28 +86,34 @@ def fast_collect_comparison(model, doc, dataset, max_samples=None):
         outputs[i] = [encode_v1[0], encode_a1[0], l1]
 
     for i in tqdm(range(max_samples)):
+        same_frame_similarity, sequence_similarity,in_identity_similarity, \
+            between_identities_similarity = [], [], [], []
         for j in range(i, max_samples):
             encode_v1, encode_a1, l1 = outputs[i]
             encode_v2, encode_a2, l2 = outputs[j]
             if i == j:
                 C11 = pairwise_cosine_similarity(encode_v1, encode_a1)
-                doc["cosine/same_frame_similarity"].extend(
-                    C11.diag().flatten().tolist())
+                same_frame_similarity.extend(C11.diag().flatten().tolist())
                 rows, cols = C11.shape
-                sequence_similarity = [C11[a][b] for b in range(cols) for a
+                sequence_similarity.extend([C11[a][b] for b in range(cols) for a
                                        in range(rows) if
-                                       a != b]
-                doc["cosine/sequence_similarity"].extend(
-                    sequence_similarity)
+                                       a != b])
             else:
                 C12 = pairwise_cosine_similarity(encode_v1, encode_a2)
                 C21 = pairwise_cosine_similarity(encode_v2, encode_a1)
                 if l1 == l2:
-                    category = "cosine/in_identity_similarity"
+                    in_identity_similarity.extend(C12.flatten().tolist())
+                    in_identity_similarity.extend(C21.flatten().tolist())
                 else:
-                    category = "cosine/between_identities_similarity"
-                doc[category].extend(C12.flatten().tolist())
-                doc[category].extend(C21.flatten().tolist())
+                    between_identities_similarity.extend(C12.flatten().tolist())
+                    between_identities_similarity.extend(C21.flatten().tolist())
+
+        doc["cosine/same_frame_similarity"].extend(same_frame_similarity)
+        doc["cosine/sequence_similarity"].extend(sequence_similarity)
+        doc["cosine/in_identity_similarity"].extend(in_identity_similarity)
+        doc["cosine/between_identities_similarity"].extend(between_identities_similarity)
+
+
 
 
 def get_model(path_to_load, num_frames=3):
